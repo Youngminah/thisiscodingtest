a058a3ef58072e7ac01118fbd40ea235bc2dd820
Youngminah <bluebelsmudi@gmail.com>
2021-05-03 23:52:00 +0900

    17업뎃

--- 3장.알고리즘유형별기출문제/13.DFSBFS문제/17.py
+++ 3장.알고리즘유형별기출문제/13.DFSBFS문제/17.py
@@ -1,55 +1,43 @@
 from collections import deque
-import copy
-import sys
-input = sys.stdin.readline
-
-def infection(graph, next, j, z, n, w):
-    queue = deque()
-    queue.append((j,z))
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
-    while queue:
-        j, z = queue.popleft()
-
-        for i in range(4):
-            nx = j + dx[i]
-            ny = z + dy[i]
-
-            if nx < 0 or ny < 0 or nx >= n or ny >= n:
-                continue
-
-            if graph[nx][ny] != 0:
-                continue
-
-            graph[nx][ny] = graph[j][z]
-            next[w].append((nx,ny))
-
-    return graph, next
 
 n, k = map(int, input().split())
 
 graph = []
+data = []
 
 for i in range(n):
     graph.append(list(map(int, input().split())))
+    for j in range(n):
+        if graph[i][j] != 0:
+            data.append((graph[i][j], 0, i, j))
+
+
+data.sort()
+q = deque(data)
+print(q)
 
 s, x, y = map(int, input().split())
 
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
 
-virus = [[] for _ in range(k + 1)]
-for j in range(n):
-    for z in range(n):
-        if graph[j][z] != 0 :
-            virus[graph[j][z]].append((j, z))
+while q:
+    virus, ps, px, py = q.popleft()
 
-for _ in range(s):
+    if ps == s:
+        break
 
-    next = [[] for _ in range(k + 1)]
-    for w in range(1, k+1):
-        for data in virus[w]:
-            j, z = data
-            graph, next = infection(graph, next, j, z, n, w)
+    for i in range(4):
+        nx = n + dx[i]
+        ny = n + dy[i]
+        
+        if nx < 0 or ny < 0 or nx>=n or ny >=n:
+            continue
 
-    virus = next
+        if graph[ny][ny] != 0 :
+            continue
+
+        graph[nx][ny] = virus
+        q.append((virus, s+1, nx, ny))
+
 print(graph[x-1][y-1])
-#print(graph)
